749f07fc30ae2aa4fad697a1c843eb7a2261a67d
mauriciolobo <mauriciolobo@gmail.com>
2020-02-09 10:39:46 -0500

    Enabling cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req,res)=>{
     res.send('GET OK')
