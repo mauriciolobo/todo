99c30be9c3534cf51776634f796f4b2e037a1474
mauriciolobo <mauriciolobo@gmail.com>
2020-02-16 06:55:20 -0500

    Changing res.JSON to res.SEND

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const express = require('express')
-const cors = require('cors')
+const DbStore = require('nedb')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+const db = new DbStore({ autoload: true, filename: 'todo' })
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
     res.header('Access-Control-Allow-Origin', '*');
     next();
-  });
+});
 app.use(express.json())
 
-app.get('/', (req,res)=>{
-    res.send('the server is up and accessible')
+function createTodo(req, data) {
+    return {
+      title: data.title,
+      order: data.order,
+      completed: data.completed || false,
+      url: req.protocol + '://' + req.get('host') + '/' + data.id
+    };
+}
+
+app.get('/', (req, res) => {
+    db.find({}, (err, data) => {
+        return res.send(data)
+    })
 })
 
-app.post('/', (req,res)=>{
-    res.json({message:'POST OK', ...req.body})
+app.post('/', (req, res) => {
+    db.insert({
+        ...req.body,
+        completed: false,
+        url: req.protocol + '://' + req.get('host') + '/' + data.id
+    },
+        (err, data) => { return res.send(data) })
 })
 
-app.patch('/', (req,res)=>{
-    res.json({message:'PATCH OK'})
+app.patch('/:id', (req, res) => {
+    res.send({ message: 'PATCH OK' })
 })
 
-app.delete('/', (req,res)=>{
-    res.json([])
+app.delete('/', (req, res) => {
+    res.send([])
 })
 
-app.listen(PORT, ()=>{console.log('Server is running...')})
+app.listen(PORT, () => { console.log('Server is running...') })
